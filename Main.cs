1c2961c81bc534ee88213ef394952a56c761e615
Matthew Nock <thematt.nock@gmail.com>
2024-05-27 09:26:30 +0100

    button signals

--- Main.cs
+++ Main.cs
@@ -1,8 +1,13 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class Main : Node2D
 {
+	private void _on_button_pressed()
+	{
+		GD.Print("Button pressed");
+	}
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -12,18 +17,7 @@ public partial class Main : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-        if (Input.IsActionJustPressed("click"))
-        {
-            Vector2 mousePos = GetGlobalMousePosition();
-			var query = new PhysicsPointQueryParameters2D();
-			query.Position = mousePos;
-            Godot.Collections.Array<Godot.Collections.Dictionary> result = GetWorld2D().DirectSpaceState.IntersectPoint(query);
 
-            if (result.Count > 0)
-            {
-                Node clickedNode = (Node)result[0]["collider"];
-                GD.Print("Mouse clicked on: " + clickedNode.Name);
-            }
-        }
 	}
 }
+
