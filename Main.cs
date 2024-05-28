89f461b75d0050df2700555efe0ceedd07f9b0dd
Matthew Nock <thematt.nock@gmail.com>
2024-05-28 18:13:43 +0100

    connect buttons

--- Main.cs
+++ Main.cs
@@ -7,8 +7,8 @@ public partial class Main : Node2D
 	[Export]
 	public PackedScene fenceScene;
 	[Export]
-	public Godot.Button[] button;
-	private void _on_button_pressed()
+	public Godot.Button[] buttons;
+	private Callable _on_button_pressed()
 	{
 		GD.Print("Button pressed");
 		var aa = fenceScene.Instantiate<Node2D>();
@@ -16,11 +16,16 @@ public partial class Main : Node2D
 		aa.Position = new Vector2(225, 175);
 		GD.Print(aa.Position);
 		AddChild(aa);
+		return new Callable();
 	}
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		foreach (var Button in buttons)
+		{
+			Button.Connect("pressed", _on_button_pressed());
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
